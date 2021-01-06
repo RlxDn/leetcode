4745e41209f1cb8239326147acc9783280085463
RlxDn <colin12212004@gmail.com>
2021-01-06 14:13:02 +0900

    2020.1.6

--- Shuffle_String.py
+++ Shuffle_String.py
@@ -1,7 +1,7 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         k=0
-        res=[for i in range(len(s))]
+        res=[0 for i in range(len(s))]
         for i in indices:
             res[i]=s[k]
             k+=1
